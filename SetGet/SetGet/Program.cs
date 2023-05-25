2ef107a735ef47c4edb8ac015fddaefda02a9c22
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:46:49 +0400

    indexer/SetGet

---
+++ SetGet/SetGet/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SetGet
+{
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            phoneBook pb = new phoneBook(8);
+            pb.SetValue(0, "aaa", "2868");
+            pb.GetValue("aaa");
+            Console.WriteLine(pb.GetValue("aaa"));
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SetGet/SetGet/phoneBook.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetGet
+{
+    internal class phoneBook
+    {
+        private string[] names;
+        private string[] numbers;
+        private int Size;
+      
+        public phoneBook(int size) {
+            this.Size = size;
+        this.names = new string[this.Size];
+          this.numbers = new string[this.Size];
+       
+        }
+
+        public void SetValue(int index, string name, string num) {
+
+            if ((index>0)&& (index < Size))
+            {
+
+                this.names[index] = name;
+                this.numbers[index] = num;  
+            }
+
+        }
+        public string GetValue(string name)
+
+        {
+
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (names[i] == name)
+                {
+                    return this.numbers[i];
+                }
+
+
+            }
+            return "faild";
+
+        }
+
+
+    }
+}
+//for (int i = 0; i < this.Size; i++) {
+
+//    Console.WriteLine("Enter your name");
+//    name[i] = Console.ReadLine();
+
+//    Console.WriteLine("Enter your name");
+//    num[i] = Console.ReadLine();
+
+
+
+//}
+
+//Console.WriteLine("Enter the size: ");
+//int length = Console.ReadLine();
+// phoneBook pb = new phoneBook(length);

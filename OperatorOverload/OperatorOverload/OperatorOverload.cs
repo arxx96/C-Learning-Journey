80e6d04838d5f5cf9a24266229f97e7cdd0661f7
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:52:26 +0400

    OperatorOverload

---
+++ OperatorOverload/OperatorOverload/OperatorOverload.cs
@@ -0,0 +1,54 @@
+﻿namespace OperatorOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            // operator overload 
+
+            sequare seq =new sequare(24);
+            sequare seq1 = new sequare(4);
+            sequare seq2 = seq * seq1;
+            Console.WriteLine(seq2.size);
+
+            sequare seq3 = seq / seq2;
+            Console.WriteLine(seq3.size);
+        }
+    }
+
+    internal class sequare
+    {
+
+        public double size;
+
+
+        public sequare(double size)
+        {
+            this.size = size;
+        }
+        // operator overload 
+        public static sequare operator *(sequare s1, sequare s2)
+        {
+
+            double value = s1.size * s2.size;
+            return new sequare(value);
+
+
+
+        }
+
+        public static sequare operator /(sequare s1, sequare s2)
+        {
+
+            double value = s1.size / s2.size;
+            return new sequare(value);
+
+
+
+        }
+
+
+    }
+
+}

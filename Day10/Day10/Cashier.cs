09f72a39fe67833824333354891fd7768dfbfb30
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:43:22 +0400

    inheritance/interface

---
+++ Day10/Day10/Cashier.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    class Cashier
+    {
+
+        //use Ipayment interface
+        public Ipayment Ipayment { get; set; }
+
+        //constructor 
+        public Cashier(Ipayment Ipayment) {
+            this.Ipayment = Ipayment;
+
+        }
+
+        public void Checkout()
+        {
+            Ipayment.Pay();
+        }
+}
+    interface Ipayment{
+           void Pay();
+}
+    // inheritance
+    class cash : Ipayment
+    {
+        public decimal amount { get; set; }
+        public cash(decimal amount) {
+            this.amount=amount;
+        
+        }
+        public void Pay()
+        {
+            Console.WriteLine(amount);
+        }
+    }
+    class visa : Ipayment
+    {
+        public decimal amount { get; set; }
+        public visa(decimal amount)
+        {
+            this.amount=amount;
+        }
+        public void Pay()
+        {
+            Console.WriteLine(amount);
+        }
+    }
+    class MasterCard : Ipayment
+    {
+        public decimal amount { get; set; }
+        public MasterCard(decimal amount)
+        {
+            this.amount=amount;
+        }
+        public void Pay()
+        {
+            Console.WriteLine(amount);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Day10/Day10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // MasterCard masterCard=new MasterCard(239m);
+           // masterCard.Pay();
+
+            Cashier cash = new Cashier(new cash(999982));
+            cash.Checkout();
+
+
+
+
+        }
+    }
+}

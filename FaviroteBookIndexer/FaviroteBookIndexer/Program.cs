c78438e9a956a8a51982375b87b5f4b246cfae62
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:56:35 +0400

    FaviroteBookIndexer

---
+++ FaviroteBookIndexer/FaviroteBookIndexer/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FaviroteBookIndexer
+{
+    internal class Program
+    {
+        //indexer
+        //when enter the book name it will return the author name
+        static void Main(string[] args)
+        {
+         
+            faviroteBook fBook=new faviroteBook(3);
+            fBook[0, "book1"] = "arwa";
+            fBook[1, "book2"] = "shrooq";
+            fBook[2, "book3"] = "alaina";
+
+            Console.WriteLine(fBook["book1"]);
+            Console.WriteLine(fBook["book2"]);
+            Console.WriteLine(fBook["book3"]);
+
+        }
+    }
+
+
+
+    
+
+
+}
---
+++ FaviroteBookIndexer/FaviroteBookIndexer/faviroteBook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaviroteBookIndexer
+{
+
+    //class faviroteBook contain  > BookName,AuthorName, index
+    internal class faviroteBook
+    {  
+            private string[] BookName;
+            private string[] AuthorName;
+            private int index;
+
+        // ctor 
+        //constructor 
+        public faviroteBook(int index)
+        {
+            this.index = index;
+            this.BookName=new string[index];
+            this.AuthorName = new string[index];
+            
+        }
+        //indexer
+        public string this[int len, string BN]
+        {
+            set
+            {
+                BookName[len] = BN;
+                AuthorName[len] = value;
+            }
+            get
+            {
+                string res = $"the book name is {BN} the author is {AuthorName[len]}";
+                return res;
+            }
+        }
+        //indexer to search for book name
+        public string this[string Bookname]
+        {
+            get
+            {
+                for (int i = 0; i < BookName.Length; i++)
+
+                {
+                    if (this.BookName[i] == Bookname)
+
+                        return "book name: " + this.BookName[i] + "  author:  " + this.AuthorName[i];
+                }
+
+                return "!!!!!!";
+
+            }
+
+        }
+    }
+}

6ecae081f8a9fa23b979005f21fc69a36db2f4ac
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:48:27 +0400

    indexr

---
+++ indexr/indexr/PhoneBook.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexr
+{
+    internal class PhoneBook
+    {
+        private string[] names;
+        private string[] numbers;
+        private int Size;
+
+        public PhoneBook(int size)
+        {
+            this.Size = size;
+            this.names = new string[this.Size];
+            this.numbers = new string[this.Size];
+
+        }
+
+
+
+        //indexr
+        public string this[int index, string name]
+        {
+
+            get
+            {
+
+                for (int i = 0; i < this.Size; i++)
+                {
+                    if (this.names[i] == name)
+                    {
+                        return numbers[i];
+                    }
+
+                }
+                return "falid";
+            }
+            set
+            {
+                numbers[index] = value;
+                this.names[index] = name;
+
+            }
+
+
+        }
+
+        public string this[string name]
+        {
+
+            get
+            {
+
+                for (int i = 0; i < this.Size; i++)
+                {
+                    if (this.names[i] == name)
+                    {
+                        return this.numbers[i];
+                    }
+
+                }
+                return "falid";
+            }
+
+
+
+        }
+
+
+
+        public void SetValue(int index, string name, string num)
+        {
+
+            if ((index > 0) && (index < Size))
+            {
+
+                this.names[index] = name;
+                this.numbers[index] = num;
+            }
+
+        }
+        public string GetValue(string name)
+
+        {
+
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (names[i] == name)
+                {
+                    return this.numbers[i];
+                }
+
+
+            }
+            return "faild";
+
+        }
+    }
+}
---
+++ indexr/indexr/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace indexr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Using in Main 
+
+
+            PhoneBook phone =new PhoneBook(2);
+            phone[0, "arwa"] = "95678351";
+            phone[1, "Safiya"] = "98745624";
+            Console.WriteLine(phone.GetValue("Safiya"));
+
+
+
+
+        }
+    }
+}

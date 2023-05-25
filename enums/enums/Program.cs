5542ca725da17e74aaadf3bc3dcebff6a860ba78
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:49:46 +0400

    enums

---
+++ enums/enums/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace enums
+{
+    internal class Program
+    {
+
+
+
+        //Enum of days
+        public enum Days
+        {
+            Sunday=1,
+            Monday,
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Creating instance of enum
+
+                        Days days = Days.Tuesday;
+            Console.WriteLine((int)days);
+
+
+        }
+    }
+}

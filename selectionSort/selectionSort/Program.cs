95ded4b53eca5660a5e553399fa50de46901b07b
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:44:36 +0400

    selectionSort

---
+++ selectionSort/selectionSort/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace selectionSort
+{
+    public class Program
+    {
+
+
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+              
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+
+                
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+            int[] arr = { 9, 1, 8, 5, 3, 2, 6, 7, 4 };
+            Console.WriteLine("Array before sorting:");
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+
+            SelectionSort(arr);
+
+            Console.WriteLine("Array after sorting:");
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}

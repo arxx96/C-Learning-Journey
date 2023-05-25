cb0090542de820f1b176d328e9d261fbf7e08561
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-05-25 16:23:35 +0400

    class implementation

---
+++ Day41/Day41/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day41
+{
+
+    //class implmintation
+    public class Employee
+    {
+        
+
+      
+
+
+        public const double TAX = 0.03;
+            public string fName;
+            public string lName;
+            public double wage;
+            public double loggedHours;
+
+         
+      
+
+    }
+    }
+
---
+++ Day41/Day41/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using static Day41.Program;
+
+namespace Day41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Employee em1 = new Employee();
+            Console.WriteLine("Enter the number of employees:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] emp_num = new int[num];
+            foreach (int i in emp_num)
+            {
+                Console.WriteLine("Enter your First Name:");
+                em1.fName = Console.ReadLine();
+                Console.WriteLine("Enter your Last Name:");
+                em1.lName = Console.ReadLine();
+                Console.WriteLine("Enter the wage:");
+                em1.wage = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the loggedHours:");
+                em1.loggedHours = double.Parse(Console.ReadLine());
+                double totalSalary = em1.wage * em1.loggedHours;
+                double netSalary = totalSalary - Employee.TAX;
+                Console.WriteLine($"Hello {em1.fName} {em1.lName} your salary is {netSalary}");
+
+               
+
+            }
+
+           
+           
+
+
+
+        }
+      
+    }
+
+}

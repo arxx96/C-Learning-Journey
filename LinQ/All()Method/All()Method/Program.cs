eee0531a0673bc24e6a86dba200d52792797e32d
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:02:37 +0400

    Add files via upload

---
+++ LinQ/All()Method/All()Method/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace All__Method
+{
+    internal class Program
+    {
+        class ProductStore
+        {
+            public string productName { get; set; }
+            public int productPrice { get; set; }
+            public int inStock { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+            // Creating List
+            IList<ProductStore> productList = new List<ProductStore>();
+
+
+
+            productList.Add(new ProductStore { productName = "iphone 12", productPrice = 1280, inStock = 40 });
+            productList.Add(new ProductStore { productName = "samsung", productPrice = 3000, inStock = 20 });
+            productList.Add(new ProductStore { productName = "hwawi", productPrice = 3500, inStock = 38 });
+            productList.Add(new ProductStore { productName = "iphone 14", productPrice = 2450,    inStock=23 });
+            productList.Add(new ProductStore { productName = "samsung note", productPrice = 7680, inStock = 19 });
+
+
+
+
+            //LINQ Method Syntax
+            var result = productList.All(p => p.inStock > 10);
+            Console.Write("All Product in Stock: ");
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

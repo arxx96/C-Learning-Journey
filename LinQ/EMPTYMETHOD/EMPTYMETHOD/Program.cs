ccb06b31448dc0495f1f184d20af975836405648
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:12:55 +0400

    Add files via upload

---
+++ LinQ/EMPTYMETHOD/EMPTYMETHOD/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EMPTYMETHOD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emptyNumber = Enumerable.Empty<string>();
+            foreach (var num in emptyNumber)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}

427793605ac79f12a23b5f5c0b7ea9a01a778d9c
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:00:04 +0400

    LinQ

---
+++ LinQ/RangeMethod/RangeMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RangeMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> squareNumber = Enumerable.Range(2, 4).Select(x => x * x);
+            foreach (var num in squareNumber)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}

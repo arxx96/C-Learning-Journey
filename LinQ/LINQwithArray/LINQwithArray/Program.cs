05c70907db67f3756872f852d05bd76cc547c58e
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:06:48 +0400

    Add files via upload

---
+++ LinQ/LINQwithArray/LINQwithArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace LINQwithArray
+{
+    internal class Program
+    {
+        class ProductStore
+        {
+            public string productName { get; set; }
+            public int productPrice { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            ArrayList productList = new ArrayList();
+
+            productList.Add(new ProductStore { productName = "iphone 12", productPrice = 1280 });
+            productList.Add(new ProductStore { productName = "samsung", productPrice = 3000 });
+            productList.Add(new ProductStore { productName = "hwawi", productPrice = 3500 });
+            productList.Add(new ProductStore { productName = "iphone 14", productPrice = 2450 });
+            productList.Add(new ProductStore { productName = "samsung note", productPrice = 7680 });
+
+
+
+            //Method 1: Query Expression
+            var search = from ProductStore p in productList
+                         where p.productName.Contains("iphone")
+                         select p;
+
+
+
+            foreach (var result in search)
+            {
+                Console.WriteLine("Product Name: {0}, Price: {1}", result.productName, result.productPrice);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LinQ/REPEATMETHOD/REPEATMETHOD/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace REPEATMETHOD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> repeatNumber = Enumerable.Repeat(5, 10);
+            foreach (var num in repeatNumber)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}

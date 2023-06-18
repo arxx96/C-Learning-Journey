038e4abb18e220e52bc7b99e4eafc9266e8db1b2
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:17:20 +0400

    Add files via upload

---
+++ LinQ/LinQ/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //where 
+            IList<string> productList = new List<string>()
+            {
+                "arwa abdullah",
+                "shrooq said",
+                "alaina dawod",
+                "said",
+                "abdullah said",
+                "mohmmed",
+                "bushra"
+            };
+
+            //Filtering Data
+            var result = from s in productList
+                         where s.Contains("abdullah")
+                         select s;
+
+            //Printing Result
+            foreach (string str in result)
+            {
+                Console.WriteLine(str.ToString());
+            }
+            Console.ReadKey();
+
+        }
+
+    
+
+    }
+    }

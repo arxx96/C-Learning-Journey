6b53c0e252bddb6279db00ca9a5cc387bf6b5d34
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-18 22:08:21 +0400

    Add files via upload

---
+++ LinQ/THENBYDESCENDING/THENBYDESCENDING/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace THENBYDESCENDING
+{
+    internal class Program
+    {
+        class ProductStore
+        {
+            public string productName { get; set; }
+            public int productPrice { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // Creating List
+            IList<ProductStore> productList = new List<ProductStore>();
+
+
+            productList.Add(new ProductStore { productName = "iphone 12", productPrice = 1280 });
+            productList.Add(new ProductStore { productName = "samsung", productPrice = 3000 });
+            productList.Add(new ProductStore { productName = "hwawi", productPrice = 3500 });
+            productList.Add(new ProductStore { productName = "iphone 14", productPrice = 2450 });
+            productList.Add(new ProductStore { productName = "samsung note", productPrice = 7680 });
+
+
+            var result = productList.OrderBy(p => p.productName).ThenByDescending(p => p.productPrice);
+
+            foreach (var list in result)
+            {
+                Console.WriteLine("Product Name: {0} | Product Price : {1}", list.productName, list.productPrice);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ LinQ/ToLookupOperator/ToLookupOperator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ToLookupOperator
+{
+    internal class Program
+    {
+
+        class ProductStore
+        {
+            public int category { get; set; }
+            public string productName { get; set; }
+
+            public string type { get; set; }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            // Creating List
+            IList<ProductStore> productList = new List<ProductStore>();
+
+            productList.Add(new ProductStore { category = 1, productName = "iphone 12", type = "new" });
+            productList.Add(new ProductStore { category = 2, productName = "samsung", type = "old" });
+            productList.Add(new ProductStore { category = 1, productName = "hwawi", type = "new" });
+            productList.Add(new ProductStore { category = 1, productName = "samsung note", type = "new" });
+            productList.Add(new ProductStore { category = 2, productName = "iphone 11", type = "old" });
+
+
+            var result = productList.ToLookup(p => p.category);
+
+            foreach (var group in result)
+            {
+                Console.WriteLine(string.Format("Category: {0}", group.Key));
+                foreach (var name in group)
+                {
+                    Console.WriteLine(string.Format("\tProduct Name: {0} | Type: {1}", name.productName, name.type));
+                }
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}

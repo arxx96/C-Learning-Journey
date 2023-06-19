e98fae4063b14aefafa83cdacabdbed92432428d
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-19 12:00:32 +0400

    Anonymous data type

---
+++ anonymous/anonymous/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace anonymous
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+           
+            //way1
+            var r = lst.Where(i => i % 2 == 0);
+            foreach (var item in r)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(" ");
+
+            //way2
+
+            var rql = from i in lst
+                      where i % 2 == 0
+                      select i;
+            foreach (var item in rql)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(" ");
+
+        }
+
+
+
+    }
+}

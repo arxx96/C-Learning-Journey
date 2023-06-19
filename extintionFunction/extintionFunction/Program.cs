d1283cc543142888f9e75771a775fa140dd12e57
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-19 12:03:07 +0400

    extintionFunction

---
+++ extintionFunction/extintionFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace extintionFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int x=1234;
+            int y=x.miror();
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ extintionFunction/extintionFunction/extenion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extintionFunction
+{
+    internal static class extenion
+    {
+
+
+        public static int miror(this int x)
+        {
+            char[] z = x.ToString().ToCharArray();
+            Array.Reverse(z);
+            return int.Parse(new String(z));
+
+
+        }
+
+
+    }
+}
